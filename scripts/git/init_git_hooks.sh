9092ef9685ac0bb2e452ef01055ed84ac6d67c3e
Maxi Rosson <maxirosson@gmail.com>
2019-07-23 22:57:46 -0300

    Project config upgrade

---
+++ scripts/git/init_git_hooks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function configure_hook {
+
+    commit_msg_link=.git/hooks/$1
+
+    # Create symbolic link if needed
+    if [[ ! -L $commit_msg_link ]];
+    then
+
+        if [[ -f $commit_msg_link ]];
+        then
+            rm $commit_msg_link
+        fi
+
+        current_dir=$(pwd)
+        ln -s "${current_dir}/scripts/git/$1" $commit_msg_link
+
+        chmod +x "${current_dir}/scripts/git/$1"
+
+    fi
+}
+
+configure_hook "commit-msg"
