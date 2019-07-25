3833ca3e4c823730076c2ca71649f538022758da
Maxi Rosson <maxirosson@gmail.com>
2019-07-25 13:06:01 -0300

    More test

--- releases-hub-gradle-plugin/src/test/java/com/releaseshub/gradle/plugin/task/DependenciesParserTest.kt
+++ releases-hub-gradle-plugin/src/test/java/com/releaseshub/gradle/plugin/task/DependenciesParserTest.kt
@@ -31,10 +31,32 @@ class DependenciesParserTest {
 	}
 
 	@Test
-	fun upgradeTest() {
+	fun upgradeCommentTest() {
 		val artifactsToUpgrade = mutableListOf<Artifact>()
 		val line = "// this is a comment"
 		val upgradeResult = UpgradeResult(false, null, null, line)
+		Assert.assertEquals(upgradeResult, DependenciesParser.upgradeDependency(line, artifactsToUpgrade))
+
+		val artifact = Artifact()
+		artifact.groupId = "a"
+		artifact.artifactId = "b"
+		artifact.latestVersion = "1.0.0"
+		artifactsToUpgrade.add(artifact)
+		Assert.assertEquals(upgradeResult, DependenciesParser.upgradeDependency(line, artifactsToUpgrade))
+	}
+
+	@Test
+	fun notUpgradeTest() {
+		val artifactsToUpgrade = mutableListOf<Artifact>()
+		val line = """libs.jdroid_java_core = "com.jdroidtools:jdroid-java-core:2.0.0""""
+		val upgradeResult = UpgradeResult(false, null, null, line)
+		Assert.assertEquals(upgradeResult, DependenciesParser.upgradeDependency(line, artifactsToUpgrade))
+
+		val artifact = Artifact()
+		artifact.groupId = "a"
+		artifact.artifactId = "b"
+		artifact.latestVersion = "1.0.0"
+		artifactsToUpgrade.add(artifact)
 		Assert.assertEquals(upgradeResult, DependenciesParser.upgradeDependency(line, artifactsToUpgrade))
 	}
 }
