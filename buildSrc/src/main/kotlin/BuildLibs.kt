e50ed646693c68eff05bbbb43b4b39bee08d9bd2
Maxi Rosson <maxirosson@gmail.com>
2019-09-01 14:45:54 -0300

    Upgrade to stable dependencies

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,8 +1,8 @@
 object BuildLibs {
     // https://github.com/maxirosson/jdroid-gradle-plugin/releases
-    const val jdroid_gradle_project_plugin = "com.jdroidtools:jdroid-gradle-project-plugin:2.1.0-SNAPSHOT"
-    const val jdroid_gradle_root_plugin = "com.jdroidtools:jdroid-gradle-root-plugin:2.1.0-SNAPSHOT"
+    const val jdroid_gradle_project_plugin = "com.jdroidtools:jdroid-gradle-project-plugin:2.1.0"
+    const val jdroid_gradle_root_plugin = "com.jdroidtools:jdroid-gradle-root-plugin:2.1.0"
 
     // https://github.com/maxirosson/jdroid-component-builder/releases
-    const val jdroid_component_builder = "com.jdroidtools:jdroid-component-builder:2.1.0-SNAPSHOT"
+    const val jdroid_component_builder = "com.jdroidtools:jdroid-component-builder:2.1.0"
 }
--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,10 +1,10 @@
 object Libs {
 
     // https://github.com/maxirosson/jdroid-java/releases
-    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.0.0-SNAPSHOT"
+    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.0.0"
 
     // https://github.com/maxirosson/jdroid-java-http/releases
-    const val jdroid_java_http_okhttp = "com.jdroidtools:jdroid-java-http-okhttp:3.0.0-SNAPSHOT"
+    const val jdroid_java_http_okhttp = "com.jdroidtools:jdroid-java-http-okhttp:3.0.0"
 
     const val jdroid_java_github = "com.jdroidtools:jdroid-java-github:1.0.0"
 
