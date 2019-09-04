537aa8cc60f7c420f673dac7c909ce8806b0dd2d
Maxi <maxirosson@gmail.com>
2019-09-03 23:38:46 -0300

    Added comment

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -9,5 +9,6 @@ object Libs {
     // https://github.com/maxirosson/jdroid-java-github/releases
     const val jdroid_java_github = "com.jdroidtools:jdroid-java-github:1.0.0"
 
+    // https://github.com/junit-team/junit4/releases
     const val junit = "junit:junit:4.12"
 }
