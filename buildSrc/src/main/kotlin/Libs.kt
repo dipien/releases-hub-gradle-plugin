3b39742c054a4d187b85536306012593fecb0b5e
Maxi <maxirosson@gmail.com>
2019-09-13 16:33:23 -0300

    Fixed constants format

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,14 +1,14 @@
 object Libs {
 
     // https://github.com/maxirosson/jdroid-java/releases
-    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.0.0"
+    const val JDROID_JAVA_CORE = "com.jdroidtools:jdroid-java-core:3.0.0"
 
     // https://github.com/maxirosson/jdroid-java-http/releases
-    const val jdroid_java_http_okhttp = "com.jdroidtools:jdroid-java-http-okhttp:3.0.0"
+    const val JDROID_JAVA_HTTP_OKHTTP = "com.jdroidtools:jdroid-java-http-okhttp:3.0.0"
 
     // https://github.com/maxirosson/jdroid-java-github/releases
-    const val jdroid_java_github = "com.jdroidtools:jdroid-java-github:1.0.0"
+    const val JDROID_JAVA_GITHUB = "com.jdroidtools:jdroid-java-github:1.0.0"
 
     // https://github.com/junit-team/junit4/releases
-    const val junit = "junit:junit:4.12"
+    const val JUNIT = "JUNIT:JUNIT:4.12"
 }
