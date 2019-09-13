e30d0db22825485fa6090932fde84463ada1e90b
Maxi <maxirosson@gmail.com>
2019-09-13 16:46:46 -0300

    Fixed junit dependency

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -10,5 +10,5 @@ object Libs {
     const val JDROID_JAVA_GITHUB = "com.jdroidtools:jdroid-java-github:1.0.0"
 
     // https://github.com/junit-team/junit4/releases
-    const val JUNIT = "JUNIT:JUNIT:4.12"
+    const val JUNIT = "junit:junit:4.12"
 }
