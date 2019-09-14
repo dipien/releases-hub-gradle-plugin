3146c9a1b9a3388434a31083054d381f7ecb04dd
Maxi <maxirosson@gmail.com>
2019-09-13 22:40:14 -0300

    Upgraded sample

--- sample/buildSrc/src/main/kotlin/com/sample/dependencies/BuildLibs.kt
+++ sample/buildSrc/src/main/kotlin/com/sample/dependencies/BuildLibs.kt
@@ -1,6 +1,6 @@
 package com.sample.dependencies
 
 object BuildLibs {
-	const val releases_hub_gradle_plugin = "com.releaseshub:releases-hub-gradle-plugin:0.1.0-SNAPSHOT"
-	const val kotlin_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.40"
+	const val RELEASES_HUB_GRADLE_PLUGIN = "com.releaseshub:releases-hub-gradle-plugin:0.2.0"
+	const val KOTLIN_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.40"
 }
--- sample/buildSrc/src/main/kotlin/com/sample/dependencies/Libs.kt
+++ sample/buildSrc/src/main/kotlin/com/sample/dependencies/Libs.kt
@@ -1,5 +1,5 @@
 package com.sample.dependencies
 
 object Libs {
-	const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.40"
+	const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.40"
 }
