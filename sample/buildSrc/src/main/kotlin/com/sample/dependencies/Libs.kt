0cea6a0fee4f6d3198a1873fd7b950479804a6dc
Maxi <maxirosson@gmail.com>
2019-09-13 22:57:15 -0300

    asdsad

--- sample/buildSrc/src/main/kotlin/com/sample/dependencies/Libs.kt
+++ sample/buildSrc/src/main/kotlin/com/sample/dependencies/Libs.kt
@@ -2,4 +2,5 @@ package com.sample.dependencies
 
 object Libs {
 	const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.40"
+	const val ANDROIDX_ROOM_RUNTIME = "androidx.room:room-runtime:2.0.0"
 }
