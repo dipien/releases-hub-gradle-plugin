3f5f40b8d4ced37ab3ab1f38ab1660a3f2622228
jdroid-ci <jdroidtools@gmail.com>
2020-09-20 00:06:26 +0000

    Upgraded com.jdroidtools:jdroid-java-core from 3.0.0 to 3.1.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,7 +1,7 @@
 object Libs {
 
     // https://github.com/maxirosson/jdroid-java/releases
-    const val JDROID_JAVA_CORE = "com.jdroidtools:jdroid-java-core:3.0.0"
+    const val JDROID_JAVA_CORE = "com.jdroidtools:jdroid-java-core:3.1.0"
 
     // https://github.com/maxirosson/jdroid-java-http/releases
     const val JDROID_JAVA_HTTP_OKHTTP = "com.jdroidtools:jdroid-java-http-okhttp:3.0.0"
