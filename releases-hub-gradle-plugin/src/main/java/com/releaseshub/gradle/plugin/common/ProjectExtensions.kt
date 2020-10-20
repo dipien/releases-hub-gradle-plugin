7b73b4a6dd88593114e6c297dde7d545cf196b1c
Mao <lcdsmao@gmail.com>
2020-10-20 16:56:52 +0900

    Use WeakHashMap for propertyResolverCache

--- releases-hub-gradle-plugin/src/main/java/com/releaseshub/gradle/plugin/common/ProjectExtensions.kt
+++ releases-hub-gradle-plugin/src/main/java/com/releaseshub/gradle/plugin/common/ProjectExtensions.kt
@@ -1,8 +1,9 @@
 package com.releaseshub.gradle.plugin.common
 
 import org.gradle.api.Project
+import java.util.WeakHashMap
 
-private val propertyResolverCache = mutableMapOf<Project, PropertyResolver>()
+private val propertyResolverCache = WeakHashMap<Project, PropertyResolver>()
 
 val Project.propertyResolver: PropertyResolver
     get() = propertyResolverCache.getOrPut(this) { PropertyResolver(this) }
