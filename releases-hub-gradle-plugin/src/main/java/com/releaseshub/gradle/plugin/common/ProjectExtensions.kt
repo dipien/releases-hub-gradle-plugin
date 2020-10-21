bad6ef075b9039b9718a72705ef258a81d778242
Maxi Rosson <maxirosson@gmail.com>
2020-10-20 23:18:42 -0300

    Remove propertyResolverCache. Close #93

--- releases-hub-gradle-plugin/src/main/java/com/releaseshub/gradle/plugin/common/ProjectExtensions.kt
+++ releases-hub-gradle-plugin/src/main/java/com/releaseshub/gradle/plugin/common/ProjectExtensions.kt
@@ -2,12 +2,16 @@ package com.releaseshub.gradle.plugin.common
 
 import org.gradle.api.Project
 
-private var propertyResolverCache: PropertyResolver? = null
+// TODO This is causing a memory leak
+// private var propertyResolverCache: PropertyResolver? = null
+//
+// val Project.propertyResolver: PropertyResolver
+//     get() {
+//         if (propertyResolverCache == null) {
+//             propertyResolverCache = PropertyResolver(this.rootProject)
+//         }
+//         return propertyResolverCache!!
+//     }
 
 val Project.propertyResolver: PropertyResolver
-    get() {
-        if (propertyResolverCache == null) {
-            propertyResolverCache = PropertyResolver(this)
-        }
-        return propertyResolverCache!!
-    }
+    get() = PropertyResolver(this)
