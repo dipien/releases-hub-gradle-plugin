20fe4aeb9a9551721b641a84eba5544324443aa3
dipien-ci <dipien2020@gmail.com>
2021-09-18 18:04:15 -0300

    Increased connection timeout. Send version as part of the user agent header

--- releases-hub-gradle-plugin/src/main/java/com/releaseshub/gradle/plugin/artifacts/api/HeadersAppender.kt
+++ releases-hub-gradle-plugin/src/main/java/com/releaseshub/gradle/plugin/artifacts/api/HeadersAppender.kt
@@ -5,11 +5,13 @@ import com.jdroid.java.http.HttpService
 import com.jdroid.java.http.HttpServiceProcessor
 import com.jdroid.java.http.MimeType
 import com.jdroid.java.utils.LocaleUtils
+import com.releaseshub.gradle.plugin.context.BuildConfig
+import java.util.concurrent.TimeUnit
 
 class HeadersAppender(private val userToken: String) : HttpServiceProcessor {
 
     companion object {
-        private const val USER_AGENT_HEADER_VALUE = "gradle"
+        const val USER_AGENT_HEADER_VALUE = "ReleasesHubGradlePlugin/${BuildConfig.VERSION}"
         const val USER_TOKEN_HEADER = "x-user-token"
         const val DEFAULT_USER_TOKEN_HEADER = "default"
     }
@@ -20,22 +22,14 @@ class HeadersAppender(private val userToken: String) : HttpServiceProcessor {
 
     override fun beforeExecute(httpService: HttpService) {
         httpService.setSsl(true)
+        httpService.setConnectionTimeout(TimeUnit.SECONDS.toMillis(30))
 
-        // User Agent header
         httpService.setUserAgent(USER_AGENT_HEADER_VALUE)
-
-        addLanguageHeader(httpService)
-
+        httpService.addHeader(HttpService.ACCEPT_LANGUAGE_HEADER, LocaleUtils.getAcceptLanguage())
         httpService.addHeader(HttpService.CONTENT_TYPE_HEADER, MimeType.JSON_UTF8)
         httpService.addHeader(HttpService.ACCEPT_HEADER, MimeType.JSON)
         httpService.addHeader(HttpService.ACCEPT_ENCODING_HEADER, HttpService.GZIP_ENCODING)
-
         httpService.addHeader(USER_TOKEN_HEADER, userToken)
-        // TODO We should send a useer agent header here, with the client name (releases-hub-gradle-plugin) and the version
-    }
-
-    private fun addLanguageHeader(httpService: HttpService) {
-        httpService.addHeader(HttpService.ACCEPT_LANGUAGE_HEADER, LocaleUtils.getAcceptLanguage())
     }
 
     override fun afterExecute(httpService: HttpService, httpResponse: HttpResponseWrapper) {
