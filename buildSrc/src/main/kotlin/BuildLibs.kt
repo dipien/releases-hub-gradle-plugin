ff554990c2c8afb7426194140c1ec15b1644c9e2
Maxi <maxirosson@gmail.com>
2022-07-23 18:28:58 -0300

    Upgrade dependencies

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,12 +1,18 @@
 object BuildLibs {
 
     // https://github.com/maxirosson/jdroid-gradle-plugin/blob/master/CHANGELOG.md
-    const val JDROID_GRADLE_ROOT_PLUGIN = "com.jdroidtools:jdroid-gradle-root-plugin:2.5.0-SNAPSHOT"
-    const val JDROID_GRADLE_PROJECT_PLUGIN = "com.jdroidtools:jdroid-gradle-project-plugin:2.5.0-SNAPSHOT"
+    const val JDROID_GRADLE_ROOT_PLUGIN = "com.jdroidtools:jdroid-gradle-root-plugin:3.0.0-SNAPSHOT"
+    const val JDROID_GRADLE_PROJECT_PLUGIN = "com.jdroidtools:jdroid-gradle-project-plugin:3.0.0-SNAPSHOT"
 
     // https://github.com/dipien/dipien-component-builder/blob/master/CHANGELOG.md
     const val DIPIEN_COMPONENT_BUILDER = "com.dipien:dipien-component-builder:3.2.0"
 
     // https://github.com/dipien/release-gradle-plugin/blob/master/CHANGELOG.md
-    const val RELEASE_GRADLE_PLUGIN = "com.dipien:release-gradle-plugin:1.0.0"
+    const val RELEASE_GRADLE_PLUGIN = "com.dipien:release-gradle-plugin:1.1.0"
+
+    // https://github.com/dipien/releases-hub-gradle-plugin/releases
+    const val RELEASES_HUB_GRADLE_PLUGIN = "com.dipien:releases-hub-gradle-plugin:3.1.0"
+
+    // https://github.com/dipien/semantic-version-gradle-plugin/blob/master/CHANGELOG.md
+    const val SEMANTIC_VERSION_GRADLE_PLUGIN = "com.dipien:semantic-version-gradle-plugin:1.0.0"
 }
