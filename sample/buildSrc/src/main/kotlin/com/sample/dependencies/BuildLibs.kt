65f628e4a8b7552c51d43118a1d3907151f3338e
Maxi <maxirosson@gmail.com>
2022-10-25 23:34:04 -0300

    Upgrade sample dependency

--- sample/buildSrc/src/main/kotlin/com/sample/dependencies/BuildLibs.kt
+++ sample/buildSrc/src/main/kotlin/com/sample/dependencies/BuildLibs.kt
@@ -1,6 +1,6 @@
 package com.sample.dependencies
 
 object BuildLibs {
-    const val RELEASES_HUB_GRADLE_PLUGIN = "com.dipien:releases-hub-gradle-plugin:3.2.0-SNAPSHOT"
+    const val RELEASES_HUB_GRADLE_PLUGIN = "com.dipien:releases-hub-gradle-plugin:4.0.0-SNAPSHOT"
     const val KOTLIN_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.40"
 }
