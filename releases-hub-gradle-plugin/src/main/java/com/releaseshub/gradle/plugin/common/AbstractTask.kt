61bcabd59ab88d115b3b4e9504294ec5bdc35481
Maxi Rosson <maxirosson@gmail.com>
2022-11-03 21:18:07 -0300

    Update AbstractTask.kt

--- releases-hub-gradle-plugin/src/main/java/com/releaseshub/gradle/plugin/common/AbstractTask.kt
+++ releases-hub-gradle-plugin/src/main/java/com/releaseshub/gradle/plugin/common/AbstractTask.kt
@@ -61,6 +61,17 @@ abstract class AbstractTask : DefaultTask() {
         commandExecutor = CommandExecutorImpl(logger, logLevel!!)
         gitHelper = GitHelper(commandExecutor)
         onExecute()
+        
+        println()
+        println("***********************************************************")
+        println("* You can support this project, so we can continue improving it:")
+        println("* - Donate with Bitcoin Lightning: http://alby.dipien.com/")
+        println("* - Donate cryptocurrency: http://coinbase.dipien.com/")
+        println("* - Donate with credit card: http://kofi.dipien.com/")
+        println("* - Donate on Patreon: http://patreon.dipien.com/")
+        println("* - Become a member of Medium (We will receive a portion of your membership fee): https://membership.medium.dipien.com")
+        println("* Thanks !!!")
+        println("***********************************************************")
     }
 
     @Internal
